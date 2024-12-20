1d7ace0f40a0ca8f173670c4af5ca67ec29342ef
rrh1441 <rrh1441@gmail.com>
2024-12-20 11:50:20 -0800

    updated favicon and metadata v1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "../globals.css"; // Import Tailwind styles
 
 export const metadata = {
-  title: "KiddoClassics",
+  title: "Little Lovesongs",
   description: "Create timeless, personalized songs for your child",
 };
 
